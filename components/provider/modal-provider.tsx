e89475e68e1f8a39710cc937fb3cd86ca7834330
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-01-15 23:46:08 +0530

    edit sever settings functionality added

--- components/provider/modal-provider.tsx
+++ components/provider/modal-provider.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
-import { InviteModal } from "../modals/invite-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -15,6 +16,7 @@ export const ModalProvider = () => {
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal/>
     </>
   );
 };
