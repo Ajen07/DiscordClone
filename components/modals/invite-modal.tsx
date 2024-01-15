a83e019c5b628c1bb0223cd1f9c8139a8f104342
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-01-15 21:28:53 +0530

    Invitation code functionality added

---
+++ components/modals/invite-modal.tsx
@@ -0,0 +1,92 @@
+"use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useModal } from "@/hooks/use-modal-store";
+import { Label } from "@/components/ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Check, Copy, RefreshCw } from "lucide-react";
+import { useOrigin } from "@/hooks/use-origin";
+import { useState } from "react";
+import axios from "axios";
+
+export const InviteModal = () => {
+  const { isOpen, onClose, type, data, onOpen } = useModal();
+  const origin = useOrigin();
+  const [copied, setCopied] = useState(false);
+  const [isloading, setIsloading] = useState(false);
+  const isModalOpen = isOpen && type === "invite";
+  const { server } = data;
+
+  const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(inviteUrl);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  const onNew = async () => {
+    try {
+      setIsloading(true);
+      const response = await axios.patch(
+        `/api/server/${server?.id}/invite-code`
+      );
+      onOpen("invite", { server: response.data });
+    } catch (error) {
+    } finally {
+      setIsloading(false);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Invite Friends
+          </DialogTitle>
+        </DialogHeader>
+        <div className="p-6">
+          <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+            Server Invite Link
+          </Label>
+          <div className="flex items-center mt-2 gap-x-2">
+            <Input
+              className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+              value={inviteUrl}
+              disabled={isloading}
+            />
+            <Button disabled={isloading} onClick={onCopy} size="icon">
+              {copied ? (
+                <Check className="w-4 h-4" />
+              ) : (
+                <Copy className="w-4 h-4" />
+              )}
+            </Button>
+          </div>
+          <Button
+            disabled={isloading}
+            variant="link"
+            size="sm"
+            className="text-xs text-zinc-500 mt-4"
+            onClick={onNew}
+          >
+            Generate new Link
+            <RefreshCw className="w-4 h-4 ml-2" />
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- components/provider/modal-provider.tsx
+++ components/provider/modal-provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
+import { InviteModal } from "../modals/invite-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -13,6 +14,7 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
+      <InviteModal />
     </>
   );
 };
